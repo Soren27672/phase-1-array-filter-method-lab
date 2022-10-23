ed3b8fc81fcdc1c0dea5e71e9cdd8937097ff231
Maxwell Gottesman <maxgman16@yahoo.com>
2022-10-23 16:26:22 -0400

    commit tree 6cd6654158633cb276e5a16665af2382d98e5b07

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution here
+
+const findMatching = (array,string) => array.filter(val => val.toLowerCase() === string.toLowerCase());
+
+const fuzzyMatch = (array,string) => array.filter(val => val.toLowerCase().startsWith(string.toLowerCase()));
+
+const matchName = (drivers,name) => drivers.filter(val => val.name === name);
